dfd3f50914f8f6d32ffa3a4e0c80614684ccceba
kayasthmahimahiteshkumar <kayasthmahima@gmail.com>
2025-06-19 11:25:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,6 @@ def play():
 
     return render_template('index.html', user_choice=user_choice, computer_choice=computer_choice, result=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use 5000 as fallback
+    app.run(host="0.0.0.0", port=port)
